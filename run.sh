0f3939e158e65b2ff99dc9f1c61236e67d9cf175
Zhou Wei <weiofcn@gmail.com>
2018-03-21 14:47:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 chmod 777 -Rf /var/ftp_data
 
 docker build -t ubuntu16.04/sftp-ftps .
-docker run -t -i -p 2222:2222 -v /var/ftp_data:/home/uftp ubuntu16.04/sftp-ftps
+docker run -d -p 2222:2222 -v /var/ftp_data:/home/uftp ubuntu16.04/sftp-ftps
